5aa1fdfaa9d95a32a33d0c32996b5ebee4ee4ec8
ryananshar <ansharryan@gmail.com>
2020-10-23 15:08:57 +0700

    'Init'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tugas Individu 1
+## Authors
+* **Muhammad Ryan Anshar Haryanto** - *1806147073* - *B*
---
+++ sipil/src/main/java/apap/tugas/sipil/SipilApplication.java
@@ -0,0 +1,13 @@
+package apap.tugas.sipil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipilApplication.class, args);
+	}
+
+}
